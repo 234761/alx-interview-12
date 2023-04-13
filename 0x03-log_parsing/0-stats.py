fe3f69caa8c3acdf8b460dda7840c577de222790
Dagnachew Feyisa <101374310+234761@users.noreply.github.com>
2023-04-12 18:00:35 -0700

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,39 @@
-File size: 5213
-200: 2
-401: 1
-403: 2
-404: 1
-405: 1
-500: 3
-File size: 11320
-200: 3
-301: 2
-400: 1
-401: 2
-403: 3
-404: 4
-405: 2
-500: 3
-File size: 16305
-200: 3
-301: 3
-400: 4
-401: 2
-403: 5
-404: 5
-405: 4
-500: 4
-^CFile size: 17146
-200: 4
-301: 3
-400: 4
-401: 2
-403: 6
-404: 6
-405: 4
-500: 4
-Traceback (most recent call last):
-  File "./0-stats.py", line 15, in <module>
-Traceback (most recent call last):
-  File "./0-generator.py", line 8, in <module>
-    for line in sys.stdin:
-KeyboardInterrupt
-    sleep(random.random())
-KeyboardInterrupt
+#!/usr/bin/python3
+"""
+Log parsing
+"""
+
+import sys
+
+if __name__ == '__main__':
+
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
+
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
